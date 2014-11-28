4b3f6c8f241c2d5902c0cb71d34d27d4b675e381
Sven Anders Robbestad <robbestad@gmail.com>
2014-11-28 10:00:00 +0100

    Added uglify

--- gulp/tasks/vendor.js
+++ gulp/tasks/vendor.js
@@ -1,13 +1,14 @@
 var gulp = require("gulp");
 gulp.task('vendor', function() {
-  var concat = require('gulp-concat')
- 
+  var concat = require('gulp-concat'),
+	  uglify = require('gulp-uglify');
   gulp.src([
       'node_modules/react/dist/react-with-addons.js',
       'node_modules/lodash/dist/lodash.js',
       'node_modules/moment/moment.js',
       'node_modules/moment/locale/nb.js'
     ])
+	.pipe(uglify())
     .pipe(concat('vendor.js'))
     .pipe(gulp.dest('public'))
 });
